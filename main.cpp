5c1eb16bdd35878666606b99d2b7aff625866eeb
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-04-23 16:19:25 -0400

    Implement simple gradient(white to blue)

--- main.cpp
+++ main.cpp
@@ -5,7 +5,11 @@
 
 using namespace std;
 
-color ray_color(const ray &r) { return color(0, 0, 0); }
+color ray_color(const ray &r) {
+  const vec3 unit_dir = unit_vector(r.direction());
+  auto a = 0.5 * (unit_dir.y() + 1.0);
+  return (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main() {
 
