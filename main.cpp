eb0e46a4b5c0f52ea61553aa5958d5acb092cbaa
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-04-23 17:53:54 -0400

    Draw sphere

--- main.cpp
+++ main.cpp
@@ -5,7 +5,18 @@
 
 using namespace std;
 
+bool hit_sphere(const point3 &center, double radius, const ray &r) {
+  vec3 oc = center - r.origin();
+  auto a = dot(r.direction(), r.direction());
+  auto b = -2 * dot(r.direction(), oc);
+  auto c = dot(oc, oc) - radius * radius;
+  return ((b * b) - 4 * a * c) >= 0;
+}
+
 color ray_color(const ray &r) {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+    return color(1, 0, 0);
+  }
   const vec3 unit_dir = unit_vector(r.direction());
   auto a = 0.5 * (unit_dir.y() + 1.0);
   return (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
