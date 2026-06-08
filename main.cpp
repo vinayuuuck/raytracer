fd24aff39dd27d91e1cd314a7129737f18d98279
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-06-07 19:31:18 -0700

    simplify hit_sphere code

--- main.cpp
+++ main.cpp
@@ -5,17 +5,24 @@
 
 using namespace std;
 
-bool hit_sphere(const point3 &center, double radius, const ray &r) {
+double hit_sphere(const point3 &center, double radius, const ray &r) {
   vec3 oc = center - r.origin();
-  auto a = dot(r.direction(), r.direction());
-  auto b = -2 * dot(r.direction(), oc);
-  auto c = dot(oc, oc) - radius * radius;
-  return ((b * b) - 4 * a * c) >= 0;
+  auto a = r.direction().length_squared();
+  auto h = dot(r.direction(), oc);
+  auto c = oc.length_squared() - radius * radius;
+  auto discriminant = h * h - a * c;
+  if (discriminant < 0) {
+    return -1.0;
+  } else {
+    return (h - sqrt(discriminant)) / a;
+  }
 }
 
 color ray_color(const ray &r) {
-  if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-    return color(1, 0, 0);
+  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+  if (t > 0) {
+    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
   }
   const vec3 unit_dir = unit_vector(r.direction());
   auto a = 0.5 * (unit_dir.y() + 1.0);
