8d4b43a0adcd4c52ca86721d87b78c3be2e38048
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-06-08 22:53:27 -0700

    Implement antialiasing

--- rtweekend.h
+++ rtweekend.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 // Cpp Std Usings
 using std::make_shared;
@@ -17,6 +18,15 @@ const double pi = 3.1415926535897932385;
 // Utility Functions
 inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+  static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  static std::mt19937 generator;
+  return distribution(generator);
+}
+inline double random_double(double min, double max) {
+  return min + (max - min) * random_double();
 }
 
 // Common Headers
