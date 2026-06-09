0e8e980cd9750ebfe290fe2b5d219e9ce0dc9274
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-06-08 21:44:48 -0700

    Move camera code into its own class

--- main.cpp
+++ main.cpp
@@ -1,62 +1,21 @@
 #include "rtweekend.h"
 
+#include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
 #include "sphere.h"
 
-color ray_color(const ray &r, const hittable &world) {
-  hit_record rec;
-  if (world.hit(r, interval(0, infinity), rec)) {
-    return 0.5 * (rec.normal + color(1, 1, 1));
-  }
-  const vec3 unit_dir = unit_vector(r.direction());
-  auto a = 0.5 * (unit_dir.y() + 1.0);
-  return (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
-
 int main() {
-
-  auto aspect_ratio = 16.0 / 9.0;
-  int image_w = 400;
-
-  int image_h = int(image_w / aspect_ratio);
-  image_h = (image_h) < 1 ? 1 : image_h;
-
-  // World
 
   hittable_list world;
 
   world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
   world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
-  auto focal_length = 1.0;
-  auto viewport_h = 2.0;
-  auto viewport_w = viewport_h * (double(image_w) / image_h);
-  auto camera_center = point3(0, 0, 0);
+  camera cam;
 
-  auto viewport_u = vec3(viewport_w, 0, 0);
-  auto viewport_v = vec3(0, -viewport_h, 0);
+  cam.aspect_ratio = 16.0 / 9.0;
+  cam.image_width = 400;
 
-  auto pixel_data_u = viewport_u / image_w;
-  auto pixel_data_v = viewport_v / image_h;
-
-  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
-                             viewport_u / 2 - viewport_v / 2;
-  auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_data_u + pixel_data_v);
-
-  std::cout << "P3\n" << image_w << ' ' << image_h << "\n255\n";
-
-  for (int i = 0; i < image_h; i++) {
-    std::clog << "\rScanlines remaining" << (image_h - i) << ' ' << std::flush;
-    for (int j = 0; j < image_w; j++) {
-      auto pixel_center = pixel00_loc + (j * pixel_data_u) + (i * pixel_data_v);
-      auto ray_direction = pixel_center - camera_center;
-      ray r(camera_center, ray_direction);
-      color pixel_color = ray_color(r, world);
-
-      write_color(std::cout, pixel_color);
-    }
-  }
-
-  std::clog << "\rDone \n";
+  cam.render(world);
 }
