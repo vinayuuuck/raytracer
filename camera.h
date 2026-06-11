1f0faf66190f37eeca5f7297d4faa54e30c09808
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-06-10 22:54:21 -0700

    Fix shadow acne

--- camera.h
+++ camera.h
@@ -82,7 +82,7 @@ private:
 
     hit_record rec;
 
-    if (world.hit(r, interval(0, infinity), rec)) {
+    if (world.hit(r, interval(0.001, infinity), rec)) {
       vec3 direction = random_on_hemisphere(rec.normal);
       return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
     }
