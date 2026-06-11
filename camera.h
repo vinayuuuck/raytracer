f8f5a45a5749db4b666106469252a0256faa128a
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-06-10 22:52:29 -0700

    Add diffusion

--- camera.h
+++ camera.h
@@ -8,6 +8,7 @@ public:
   double aspect_ratio = 1.0; // Ratio of image width over height
   int image_width = 100;     // Rendered image width in pixel count
   int samples_per_pixel = 10;
+  int max_depth = 10;
 
   void render(const hittable &world) {
     initialize();
@@ -21,7 +22,7 @@ public:
         color pixel_color(0, 0, 0);
         for (int sample = 0; sample < samples_per_pixel; sample++) {
           ray r = get_ray(i, j);
-          pixel_color += ray_color(r, world);
+          pixel_color += ray_color(r, max_depth, world);
         }
         write_color(std::cout, pixel_samples_scale * pixel_color);
       }
@@ -75,12 +76,18 @@ private:
     return vec3(random_double() - 0.5, random_double() - 0.5, 0);
   }
 
-  color ray_color(const ray &r, const hittable &world) const {
+  color ray_color(const ray &r, int depth, const hittable &world) const {
+    if (depth <= 0)
+      return color(0, 0, 0);
+
     hit_record rec;
+
     if (world.hit(r, interval(0, infinity), rec)) {
-      return 0.5 * (rec.normal + color(1, 1, 1));
+      vec3 direction = random_on_hemisphere(rec.normal);
+      return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
     }
-    const vec3 unit_dir = unit_vector(r.direction());
+
+    vec3 unit_dir = unit_vector(r.direction());
     auto a = 0.5 * (unit_dir.y() + 1.0);
     return (1 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
   }
