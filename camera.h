3af14af9f4c71f69ebf475c20a85e9498c83e4fb
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-06-13 10:49:27 -0700

    Implement fuzzy metal

--- camera.h
+++ camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H
 
 #include "hittable.h"
+#include "material.h"
 
 class camera {
 public:
@@ -83,8 +84,11 @@ private:
     hit_record rec;
 
     if (world.hit(r, interval(0.001, infinity), rec)) {
-      vec3 direction = rec.normal - random_unit_vector();
-      return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
+      ray scattered;
+      color attenuation;
+      if (rec.mat->scatter(r, rec, attenuation, scattered))
+        return attenuation * ray_color(scattered, depth - 1, world);
+      return color(0, 0, 0);
     }
 
     vec3 unit_dir = unit_vector(r.direction());
