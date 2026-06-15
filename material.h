71c2a852f570d3e337e51efbe95dbf8d87619f98
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2026-06-14 20:29:00 -0700

    Implement positionable camera

--- material.h
+++ material.h
@@ -63,14 +63,31 @@ public:
     double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
 
     vec3 unit_direction = unit_vector(r_in.direction());
-    vec3 refracted = refract(unit_direction, rec.normal, ri);
+    double cos_theta = std::fmin(dot(-unit_direction, rec.normal), 1.0);
+    double sin_theta = std::sqrt(1 - cos_theta * cos_theta);
 
-    scattered = ray(rec.p, refracted);
+    vec3 direction;
+
+    if ((ri * sin_theta > 1.0) ||
+        reflectance(cos_theta, ri) > random_double()) {
+      direction = reflect(unit_direction, rec.normal);
+    } else {
+      direction = refract(unit_direction, rec.normal, ri);
+    }
+
+    scattered = ray(rec.p, direction);
     return true;
   }
 
 private:
   double refraction_index;
+
+  static double reflectance(double cosine, double refraction_index) {
+    // Schlick's approximation
+    auto r0 = (1 - refraction_index) / (1 + refraction_index);
+    r0 = r0 * r0;
+    return r0 + (1 - r0) * std::pow(1 - cosine, 5);
+  }
 };
 
 #endif
